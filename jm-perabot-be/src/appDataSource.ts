f05b3ff41b471618df592f7e9658478c93cbccc9
altschmerz <ktslcbcmkshh@gmail.com>
2025-12-26 18:03:07 +0700

    use env vars for db config

--- jm-perabot-be/src/appDataSource.ts
+++ jm-perabot-be/src/appDataSource.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm'
 
 const dataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
-  username: 'postgres',
-  password: 'password',
-  database: 'jm-perabot-dev',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['build/src/models/**/*.js'],
   logging: ['error'],
   logger: 'advanced-console',
