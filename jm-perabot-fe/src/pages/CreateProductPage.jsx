f21ce7667bc67e3992be3de5d4c87625a34ee239
altschmerz <ktslcbcmkshh@gmail.com>
2025-12-28 15:36:01 +0700

    tidy up create product page display

--- jm-perabot-fe/src/pages/CreateProductPage.jsx
+++ jm-perabot-fe/src/pages/CreateProductPage.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { useForm } from 'react-hook-form'
 import { useDispatch } from 'react-redux'
@@ -10,12 +9,8 @@ import useResourceMapper from '../hooks/useResourceMapper'
 const CreateProductPage = () => {
   const dispatch = useDispatch()
 
-  const { register, formState, handleSubmit, watch } = useForm()
+  const { register, formState, handleSubmit } = useForm()
   const formErrors = formState.errors
-
-  useEffect(() => {
-    console.log('FORM ERRORS', formErrors)
-  }, [formErrors])
 
   const onSubmit = (data) => {
     dispatch(
@@ -40,101 +35,135 @@ const CreateProductPage = () => {
   return (
     <Layout>
       <div>
-        <div className="section-title text-center mt-10">Add Product</div>
+        <div className="section-title text-center mt-10">Tambah Produk</div>
         <div className="mt-5">
           <Form>
-            <Form.Group>
-              <Form.Label className="mr-5 text-lg font-medium">
-                Kategori
-              </Form.Label>
-              <Form.Select {...register('categoryId', { required: true })}>
-                <option value="" selected>
-                  Pilih kategori
-                </option>
-                {categories?.map((category) => {
-                  return (
-                    <option key={category.id} value={category.id}>
-                      {category.name}
-                    </option>
-                  )
-                })}
-              </Form.Select>
-            </Form.Group>
-            {formErrors.categoryId && <div>Kategori wajib dipilih</div>}
-
-            <Form.Group>
-              <Form.Label className="mr-5 text-lg font-medium">Nama</Form.Label>
-              <Form.Control
-                className="border border-black focus:outline-none px-2 py-1"
-                {...register('name', { required: true })}
-              />
-            </Form.Group>
-            {formErrors.name && <div>Nama wajib diisi</div>}
-
-            <Form.Group>
-              <Form.Label className="mr-5 text-lg font-medium">Kode</Form.Label>
-              <Form.Control
-                className="border border-black focus:outline-none px-2 py-1"
-                {...register('sku', { required: true })}
-              />
-            </Form.Group>
-            {formErrors.sku && <div>Kode wajib diisi</div>}
-
-            <Form.Group>
-              <Form.Label className="mr-5 text-lg font-medium">
-                Deskripsi
-              </Form.Label>
-              <Form.Control
-                className="border border-black focus:outline-none px-2 py-1"
-                as="textarea"
-                rows={3}
-                {...register('description')}
-              />
+            <Form.Group className="mb-3">
+              <div className="flex justify-between">
+                <Form.Label className="mr-5 text-lg font-medium">
+                  Kategori
+                </Form.Label>
+                <Form.Select {...register('categoryId', { required: true })}>
+                  <option value="" selected>
+                    Pilih kategori
+                  </option>
+                  {categories?.map((category) => {
+                    return (
+                      <option key={category.id} value={category.id}>
+                        {category.name}
+                      </option>
+                    )
+                  })}
+                </Form.Select>
+              </div>
+              {formErrors.categoryId && (
+                <div className="text-red-600">Kategori wajib dipilih</div>
+              )}
             </Form.Group>
 
-            <Form.Group>
-              <Form.Label className="mr-5 text-lg font-medium">
-                Harga modal
-              </Form.Label>
-              <Form.Control
-                className="border border-black focus:outline-none px-2 py-1"
-                {...register('purchasePrice', { required: true })}
-              />
+            <Form.Group className="mb-3">
+              <div className="flex justify-between">
+                <Form.Label className="mr-5 text-lg font-medium">
+                  Nama
+                </Form.Label>
+                <Form.Control
+                  className="border border-black focus:outline-none px-2 py-1"
+                  {...register('name', { required: true })}
+                />
+              </div>
+              {formErrors.name && (
+                <div className="text-red-600">Nama wajib diisi</div>
+              )}
             </Form.Group>
-            {formErrors.purchasePrice && <div>Harga modal wajib diisi</div>}
+
+            <Form.Group className="mb-3">
+              <div className="flex justify-between">
+                <Form.Label className="mr-5 text-lg font-medium">
+                  Kode
+                </Form.Label>
+                <Form.Control
+                  className="border border-black focus:outline-none px-2 py-1"
+                  {...register('sku', { required: true })}
+                />
+              </div>
+              {formErrors.sku && (
+                <div className="text-red-600">Kode wajib diisi</div>
+              )}
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <div className="flex justify-between">
+                <Form.Label className="mr-5 text-lg font-medium">
+                  Deskripsi
+                </Form.Label>
+                <Form.Control
+                  className="border border-black focus:outline-none px-2 py-1"
+                  as="textarea"
+                  rows={3}
+                  {...register('description')}
+                />
+              </div>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <div className="flex justify-between">
+                <Form.Label className="mr-5 text-lg font-medium">
+                  Harga modal
+                </Form.Label>
+                <Form.Control
+                  className="border border-black focus:outline-none px-2 py-1"
+                  {...register('purchasePrice', { required: true })}
+                />
+              </div>
+              {formErrors.purchasePrice && (
+                <div className="text-red-600">Harga modal wajib diisi</div>
+              )}
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <div className="flex justify-between">
+                <Form.Label className="mr-5 text-lg font-medium">
+                  Harga ecer
+                </Form.Label>
+                <Form.Control
+                  className="border border-black focus:outline-none px-2 py-1"
+                  {...register('retailPrice', { required: true })}
+                />
+              </div>
+              {formErrors.retailPrice && (
+                <div className="text-red-600">Harga ecer wajib diisi</div>
+              )}
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <div className="flex justify-between">
+                <Form.Label className="mr-5 text-lg font-medium">
+                  Harga grosir
+                </Form.Label>
+                <Form.Control
+                  className="border border-black focus:outline-none px-2 py-1"
+                  {...register('wholesalerPrice', { required: true })}
+                />
+              </div>
+              {formErrors.wholesalerPrice && (
+                <div className="text-red-600">Harga grosir wajib diisi</div>
+              )}
+            </Form.Group>
 
             <Form.Group>
-              <Form.Label className="mr-5 text-lg font-medium">
-                Harga ecer
-              </Form.Label>
-              <Form.Control
-                className="border border-black focus:outline-none px-2 py-1"
-                {...register('retailPrice', { required: true })}
-              />
+              <div className="flex justify-between">
+                <Form.Label className="mr-5 text-lg font-medium">
+                  Total stok
+                </Form.Label>
+                <Form.Control
+                  className="border border-black focus:outline-none px-2 py-1"
+                  {...register('totalStock', { required: true })}
+                />
+              </div>
+              {formErrors.totalStock && (
+                <div className="text-red-600">Total stok wajib diisi</div>
+              )}
             </Form.Group>
-            {formErrors.retailPrice && <div>Harga ecer wajib diisi</div>}
-
-            <Form.Group>
-              <Form.Label className="mr-5 text-lg font-medium">
-                Harga grosir
-              </Form.Label>
-              <Form.Control
-                className="border border-black focus:outline-none px-2 py-1"
-                {...register('wholesalerPrice', { required: true })}
-              />
-            </Form.Group>
-            {formErrors.wholesalerPrice && <div>Harga grosir wajib diisi</div>}
-
-            <Form.Group>
-              <Form.Label className="mr-5 text-lg font-medium">
-                Total stok
-              </Form.Label>
-              <Form.Control
-                className="border border-black focus:outline-none px-2 py-1"
-                {...register('totalStock', { required: true })}
-              />
-            </Form.Group>
-            {formErrors.totalStock && <div>Total stok wajib diisi</div>}
 
             <Button
               className="bg-black flex items-center p-2 text-white cursor-pointer mt-5"
