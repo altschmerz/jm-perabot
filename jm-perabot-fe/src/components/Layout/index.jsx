3648c9836dfb98f46ea8821459b58c14b61a617f
altschmerz <ktslcbcmkshh@gmail.com>
2026-01-14 14:11:51 +0700

    add bottom padding in layout

--- jm-perabot-fe/src/components/Layout/index.jsx
+++ jm-perabot-fe/src/components/Layout/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import Navbar from '../Navbar'
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Navbar />
-      <div className="px-5">{children}</div>
+      <div className="px-3 pb-3">{children}</div>
       {/* <Footer /> */}
     </div>
   )
