3399556c17689c725e44ac6758f8b0ba75a66499
altschmerz <ktslcbcmkshh@gmail.com>
2026-04-11 19:16:48 +0700

    only admin can access create product page

--- jm-perabot-fe/src/pages/CreateProductPage.jsx
+++ jm-perabot-fe/src/pages/CreateProductPage.jsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button, Form, Spinner } from 'react-bootstrap'
 import { useFieldArray, useForm } from 'react-hook-form'
-import { useDispatch } from 'react-redux'
+import toast from 'react-hot-toast'
+import { PiWarningCircleBold } from 'react-icons/pi'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import fromApi from '../actions/fromApi'
 import Layout from '../components/Layout'
 import useFromApi from '../hooks/useFromApi'
 import useResourceMapper from '../hooks/useResourceMapper'
+import { ADMIN_ROLE_TYPE_ID } from '../utils/constants'
 
 const CreateProductPage = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
+
+  const authUser = useSelector((state) => state.authUser)
+
+  useEffect(() => {
+    if (window.location.pathname !== '/') {
+      if (!authUser) {
+        toast('Anda belum login. Silahkan login terlebih dahulu.', {
+          id: 'not-logged-in',
+          icon: <PiWarningCircleBold color="red" />,
+          className: 'bg-red-100',
+        })
+        navigate('/')
+        return
+      }
+
+      if (authUser.role !== ADMIN_ROLE_TYPE_ID) {
+        toast('Anda tidak memiliki akses untuk halaman ini', {
+          id: 'restricted-access',
+          icon: <PiWarningCircleBold color="red" />,
+          className: 'bg-red-100',
+        })
+        navigate('/')
+      }
+    }
+  }, [authUser, navigate])
 
   const [isLoading, setIsLoading] = useState(false)
 
