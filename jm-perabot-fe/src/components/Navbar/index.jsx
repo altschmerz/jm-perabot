1f8d43009a9a1638326f14dd05e3c7f7e8887857
altschmerz <ktslcbcmkshh@gmail.com>
2026-04-11 14:01:11 +0700

    add a user icon in navbar for login

--- jm-perabot-fe/src/components/Navbar/index.jsx
+++ jm-perabot-fe/src/components/Navbar/index.jsx
@@ -1,3 +1,6 @@
+import { FaUser } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
+
 // const TABS = [
 //   { label: 'New In', path: 'new-in' },
 //   { label: 'Sale', path: 'sale' },
@@ -5,13 +8,20 @@
 // ]
 
 const Navbar = () => {
+  const navigate = useNavigate()
   // const [selected, setSelected] = useState();
 
   return (
     <div className="sticky top-0 z-50">
-      <div className="title flex justify-center bg-zinc-100 p-4">
-        JM PERABOT
+      <div className="relative title flex justify-center items-center bg-zinc-100 p-4">
+        <div>JM PERABOT</div>
+        <FaUser
+          size={20}
+          className="absolute right-4"
+          onClick={() => navigate('/login')}
+        />
       </div>
+
       {/* <div className="flex flex-row-reverse justify-between items-center bg-white px-5 py-2"> */}
       {/* <div className="flex justify-between">
           {TABS.map((tab) => (
