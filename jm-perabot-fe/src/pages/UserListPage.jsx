09ed4a1a92d5b6f1d48405a1ea1c7622feb4d235
altschmerz <ktslcbcmkshh@gmail.com>
2026-04-13 16:57:06 +0700

    only admin can view user list page

--- jm-perabot-fe/src/pages/UserListPage.jsx
+++ jm-perabot-fe/src/pages/UserListPage.jsx
@@ -1,13 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
+import { PiWarningCircleBold } from 'react-icons/pi'
+import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import fromApi from '../actions/fromApi'
 import Layout from '../components/Layout'
 import UserRow from '../components/UserRow'
 import useFromApi from '../hooks/useFromApi'
 import useResourceMapper from '../hooks/useResourceMapper'
+import { ADMIN_ROLE_TYPE_ID } from '../utils/constants'
 
 const UserListPage = () => {
   const navigate = useNavigate()
+
+  const authUser = useSelector((state) => state.authUser)
+
+  useEffect(() => {
+    if (window.location.pathname !== '/') {
+      if (!authUser) {
+        toast('Anda belum login. Silahkan login terlebih dahulu.', {
+          id: 'not-logged-in',
+          icon: <PiWarningCircleBold color="red" />,
+          className: 'bg-red-100',
+        })
+        navigate('/')
+        return
+      }
+
+      if (authUser.role !== ADMIN_ROLE_TYPE_ID) {
+        toast('Anda tidak memiliki akses untuk halaman ini', {
+          id: 'restricted-access',
+          icon: <PiWarningCircleBold color="red" />,
+          className: 'bg-red-100',
+        })
+        navigate('/')
+      }
+    }
+  }, [authUser, navigate])
 
   const [search, setSearch] = useState('')
 
